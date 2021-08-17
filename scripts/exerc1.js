80fe41ca1f7c7cfa5ff4307a4a4f565460d887d0
devmateusramos <devmateusramos@gmail.com>
2021-08-17 11:31:22 -0300

    alter

---
+++ scripts/exerc1.js
@@ -0,0 +1,4 @@
+// Objetivo é extrair parte da tabela e as colunas, armazenando-as em variáveis
+const statement = "create table author (id number, name string, age number, city string, state string, country string)"
+console.log(statement);
+
