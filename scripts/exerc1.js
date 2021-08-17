7b9e73da10bb9edf4eaefc9c48b2a240820a287d
devmateusramos <devmateusramos@gmail.com>
2021-08-17 13:37:36 -0300

    add gp de captura, quantificador, metacaractere

--- scripts/exerc1.js
+++ scripts/exerc1.js
@@ -1,6 +1,8 @@
 // Objetivo é extrair parte da tabela e as colunas, armazenando-as em variáveis
 const statement = "create table author (id number, name string, age number, city string, state string, country string)";
-const regexp = /create table author/;
+const regexp = /create table ([\w]+)/;
 const parsedStatement = statement.match(regexp);
+const tableName = parsedStatement[1];
 console.log(parsedStatement);
+console.log(tableName);
 
