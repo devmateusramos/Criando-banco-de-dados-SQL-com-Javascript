640e3669f7786a0193b5344bb7678a0d188a0dc2
devmateusramos <devmateusramos@gmail.com>
2021-08-17 13:24:58 -0300

    add regexp

--- scripts/exerc1.js
+++ scripts/exerc1.js
@@ -1,4 +1,6 @@
 // Objetivo é extrair parte da tabela e as colunas, armazenando-as em variáveis
-const statement = "create table author (id number, name string, age number, city string, state string, country string)"
-console.log(statement);
+const statement = "create table author (id number, name string, age number, city string, state string, country string)";
+const regexp = /create table author/;
+const parsedStatement = statement.match(regexp);
+console.log(parsedStatement);
 
