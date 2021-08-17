99d66565fac3b3da5fd6335f042ccdb4e7678fc2
devmateusramos <devmateusramos@gmail.com>
2021-08-17 13:42:23 -0300

    capturando as variaveis pra column

--- scripts/exerc1.js
+++ scripts/exerc1.js
@@ -1,8 +1,10 @@
 // Objetivo é extrair parte da tabela e as colunas, armazenando-as em variáveis
 const statement = "create table author (id number, name string, age number, city string, state string, country string)";
-const regexp = /create table ([\w]+)/;
+const regexp = /create table ([\w]+) \((.+)\)/;
 const parsedStatement = statement.match(regexp);
 const tableName = parsedStatement[1];
+let column = parsedStatement[2];
 console.log(parsedStatement);
 console.log(tableName);
+console.log(column);
 
