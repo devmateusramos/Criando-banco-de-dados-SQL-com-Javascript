51e5dd5eb352044ba2823b3c2320bc0c28cca54a
devmateusramos <devmateusramos@gmail.com>
2021-09-12 17:25:57 -0300

    Exercício 6.2

--- scripts/exerc6.js
+++ scripts/exerc6.js
@@ -37,7 +37,7 @@ const database = {
         this.tables[tableName].data.push(row)
     },
     select(statement){
-
+        return [];
     },
     execute(statement) {
         if (statement.startsWith("create table")) {
