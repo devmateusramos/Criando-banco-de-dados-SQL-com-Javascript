9d706ffd38ffba474ff5997508162bd93ec0abe2
devmateusramos <devmateusramos@gmail.com>
2021-09-12 17:48:35 -0300

    Exercício 6.9 add id para os selecionados

--- scripts/exerc6.js
+++ scripts/exerc6.js
@@ -70,7 +70,7 @@ try{
     database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
     database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
     database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    console.log(JSON.stringify(database.execute("select name, age from author"), undefined, " "));
+    console.log(JSON.stringify(database.execute("select id, name, age from author"), undefined, " "));
     
 } catch(e){
     console.log(e)
