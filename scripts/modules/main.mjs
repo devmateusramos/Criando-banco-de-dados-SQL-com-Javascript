c6c4002524a93d72d612c8188617e1aee9249c10
devmateusramos <devmateusramos@gmail.com>
2021-09-20 15:48:42 -0300

    Exercício 11completo add promises

--- scripts/modules/main.mjs
+++ scripts/modules/main.mjs
@@ -4,17 +4,20 @@
 
     const database = new Database();
     database.execute("create table author (id number, name string, age number, city string, state string, country string)").then(function(){
-        return database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)").then(function(){
+        return Promise.all([
+            database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+            database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+            database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)")
+        ]).then(function(){
            return database.execute("select name, age from author").then(function(result) {
-               console.log(result);
+               console.log(JSON.stringify(result, undefined, " "));
            });
         }).catch(function(e) {
-            console.log(e);
+            console.log(e.message);
         });
     });
     
-    // database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    // database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+    
     // database.execute("delete from author where id = 2");
     // console.log(JSON.stringify(database.execute("select name, age from author"), undefined, " "));// O FATO DO AUTHOR TER Q TER ESPAÇO ERA UM ESPAÇO A MAIS NA REGEXP Q QND REMOVIDO QUEBRAVA PORQUE SÓ APÓS REMOVER PODIA POR UM DPS DO ?: SEM QUEBRAR TAMBÉM.
     
