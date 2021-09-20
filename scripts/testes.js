6a12bccf80ec6926881cfadf3981f74fdd2f8166
devmateusramos <devmateusramos@gmail.com>
2021-09-20 08:31:11 -0300

    Exercício 10 inicio

--- scripts/testes.js
+++ scripts/testes.js
@@ -1,55 +1,12 @@
-class Shape {
-    toString() {
-        return `area: ${this.calculateArea()}`
-    };
-}
-
-class Square extends Shape {
-    constructor(side) {
-        super();
-        this.side = side;
-    }
-
-    toString() {
-        return `side: ${this.side} e ${super.toString()}`;
-    }
-
-    calculateArea() {
-        return Math.pow(this.side, 2)
-    }
-
-    static fromArea(area) {
-        return new Square(Math.sqrt(area));
-    }
-
-}
-const square = Square.fromArea(25);
-
-console.log(square)
-console.log(square.toString())
-console.log("A área é: " + square.calculateArea())
-
-class Circle extends Shape{
+class Circle {
     constructor(radius) {
-        super();
         this.radius = radius;
     }
 
-    calculateArea() {
+    get area() {
         return Math.PI * Math.pow(this.radius, 2);
     }
-
-    toString() {
-        return `radius: ${this.radius} e ${super.toString()}`
-    }
-
-    static fromArea(area) {
-        return new Circle(Math.sqrt(area / Math.PI))
-    }
-
 }
 
-const circle = Circle.fromArea(314.1592653589793);
-console.log(circle);
-console.log(circle.toString())
-console.log("A área é: " + circle.calculateArea())
+const circle = Circle(10);
+console.log(circle)
